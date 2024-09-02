2a3d327c7bda41030ead9cf04a51a9f2766c7a6d
something-natural <tsglow@naver.com>
2024-09-02 15:46:57 +0900

    feat: done fucntion checkPal

--- script.js
+++ script.js
@@ -2,59 +2,34 @@
 const input = document.getElementById("text-input")
 const btn = document.getElementById("check-btn")
 const result = document.getElementById("result")
-let inputValue = ""
-
-
-// function to getInput value
-function getInput(e){
-    inputValue = e.target.value;
-}
 
 // function to check palindrome
-function checkPal(){    
-    // first, remove '\W' and make all letters to lowercase. don't use "" for regex    
+function checkPal(e){    
+    // remove '\W' and make all letters to lowercase. don't use "" for regex    
     const reg = /[a-z]|[0-9]/g    
-    const arr = inputValue.toLowerCase().match(reg);
+    const arr = input.value.toLowerCase().match(reg);
+    // declare empty array
     let firHarf = [];
     let secHarf = []; 
-    let resultMsg  
-    if ( arr.length % 2 === 0) {        
-        // use ...arr
+    let resultMsg 
+
+    // slice arr into two array
+    if ( arr.length % 2 === 0) {
         firHarf = [ ...arr.slice(0, arr.length / 2)]
-        secHarf = [ ...arr.reverse().slice(0, arr.length / 2)]        
-        /*
-        or use loop and push
-        for (let i = 0 ; i < arr.length / 2 ; i++ ){
-            firHarf.push(arr[i]);            
-        }
-        for ( let j = arr.length - 1; j = arr.length / 2 ; j--){                
-            secHarf.push(arr[j]);
-        }
-        console.log( firHarf.join(""), secHarf.join(""))
-        */
-    } else {
-        // user ...arr        
+        secHarf = [ ...arr.reverse().slice(0, arr.length / 2)]                
+    } else {        
         firHarf = [ ...arr.slice(0, Math.floor(arr.length / 2))]
         secHarf = [ ...arr.reverse().slice(0, Math.floor(arr.length / 2))]
         console.log(firHarf.join(""), secHarf.join(""))
-        /*
-        or use loop and push
-        for (let i = 0 ; i < Math.floor(arr.length / 2) ; i++ ){
-            firHarf.push(arr[i]);            
-        }
-        for ( let j = arr.length - 1; j < Math.floor(arr.length / 2) ; j--){                
-            secHarf.push(arr[j]);
-        }
-        console.log( firHarf.join(""), secHarf.join(""))
-        */
-    }
+     }
+    
+     // print result
     if ( firHarf.join("") === secHarf.join("") ){
-        resultMsg = `${inputValue} is a palindrome`
+        resultMsg = `${input.value} is a palindrome`
     } else {
-        resultMsg = `${inputValue} is not a palindrome`
+        resultMsg = `${input.value} is not a palindrome`
     }
-    result.innerHTML = `<p>${resultMsg}</p>`;
+    result.innerText = resultMsg;
 }
 
-input.addEventListener("input", getInput);
 btn.addEventListener("click", checkPal)
